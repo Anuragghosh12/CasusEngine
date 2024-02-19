182fa5b97ebb07119ecfee2be4c22cc6b37bacdb
Anurag Ghosh <anuragghosh2013@gmail.com>
2024-02-19 19:31:52 +0530

    Animation and Character Implementation

--- Casus/src/Characters/Warrior.cpp
+++ Casus/src/Characters/Warrior.cpp
@@ -4,14 +4,13 @@
 
 Warrior::Warrior(Properties *props): Character(props)
 {
-    m_Row=0;
-    m_FrameCount=6;
-    m_AnimSpeed=120;
+    m_Animation=new Animation();
+    m_Animation->SetProps(m_TextureID,0,6,120,SDL_FLIP_HORIZONTAL);
 }
 
 void Warrior::Draw()
 {
-    TextureManager::GetInstance()->DrawFrame(m_TextureID,m_Transform->X, m_Transform->Y,m_Width,m_Height,m_Row,m_Frame);
+    m_Animation->Draw(m_Transform->X,m_Transform->Y,m_Width,m_Height);
 }
 
 void Warrior::Clean()
@@ -21,5 +20,5 @@ void Warrior::Clean()
 
 void Warrior::Update(float deltaTime)
 {
-    m_Frame=(SDL_GetTicks()/m_AnimSpeed)%m_FrameCount;
+    m_Animation->Update();
 }
--- Casus/src/Characters/Warrior.h
+++ Casus/src/Characters/Warrior.h
@@ -1,5 +1,8 @@
 ﻿#pragma once
 #include "Character.h"
+#include "src/Animation/Animation.h"
+
+
 class Warrior: public Character
 {
 public:
@@ -8,5 +11,8 @@ public:
     virtual void Clean();
     virtual void Update(float deltaTime);
 private:
-    int m_Row,m_Frame,m_FrameCount,m_AnimSpeed;
+    //int m_Row,m_Frame,m_FrameCount,m_AnimSpeed;
+
+
+    Animation* m_Animation;
 };
